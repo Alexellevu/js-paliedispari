af43b7ca9763ce1546392ba5d1f3e935a3b46b7e
alexellevu <newnapster83@gmail.com>
2021-04-29 17:44:14 +0200

    correzzione funzione palindroma

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,31 @@
+//inserisco la parola
 var parola = prompt('inserisci una parola');
 console.log(parola);
-
+//creo la funzione 
 function palindroma(parola){
+//trasformo la parola in array
 var parolaArray = parola.split('');
 console.log(parolaArray);
-var parolaReverse = parolaArray.reverse();
-console.log(parolaReverse); 
-if (parolaArray == parolaReverse){
-    return alert ('la parola inserita è palindroma');
+//inverto l'ordine dell' array con reverse()
+var parolaArrayReverse = parolaArray.reverse();
+console.log(parolaArrayReverse);
+//trasformo l' array invertito nuovamente in stringa
+var parolaReverse = parolaArrayReverse.join('');
+console.log(parolaReverse);
+//confronto la parola iniziale con la stringa inversa
+if (parola == parolaReverse){
+    return true;
 }
     else{
-        return alert ('la parola inserita non è palindroma');
+        return false;
     }
 }
-
+//richiamo la funzione
 var risultato = palindroma(parola);
  
+if(risultato == true){
+    console.log('la parola è palindorma');
+}
+ else{
+     console.log('la parola non è palindroma');
+ }
